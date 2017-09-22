df081ca26803a61361116c184a48ce65fcc7c648
qing <123456>
2017-09-22 20:12:38 +0800

    Initial Commit

---
+++ GQUserLog/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GQUserLog
+//
+//  Created by qing on 2017/9/22.
+//  Copyright © 2017年 YCC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GQUserLog/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GQUserLog
+//
+//  Created by qing on 2017/9/22.
+//  Copyright © 2017年 YCC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
